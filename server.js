f9e4008d13b7cf45a17c9d09d4175e081a28310d
Carlos Avance <carlosavance@gmail.com>
2020-05-25 00:08:41 -0300

    Configurado conexão com mongodb e criado schemma

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 const express = require ('express')
+const bodyParser = require ('body-parser')
+const mongoose = require ('mongoose')
 
 const app = express()
 let port = 3000
-app.listen (port,() => {
-    console.log (`Servidor rodando na porta ${port}`)
+
+app.listen(port,() => {
+    console.log(`Servidor rodando na porta ${port}`)
 })
 
+mongoose.connect("mongodb+srv://api-node:!123456@cluster0-khd7l.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(bodyParser.json())
 
-app.get('/', (req,res)=> {
-    res.send("Minha primeira rota")
+const users = {
+
+}
+
+const User = require("./src/models/User")
+
+app.post('/', async (req,res)=> {
+
+    const { nome, cidade, idade } = req.body
+    const user = await User.create( {nome, cidade, idade} )
+
+res.json({user})
+
 })
-
-
-app.get('/teste', (req,res)=> {
-    res.send("Meu primeiro teste")
-})
-
-app.get('/teste/2', (req,res)=> {
-    res.send("Meu teste 2")
-})
-
